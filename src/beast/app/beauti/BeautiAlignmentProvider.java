f66a0e717c6980547f61b067bfcb2d0c3f33692c
Arjun Dhawan <arjun.singh.dhawan@gmail.com>
2016-06-27 12:29:30 +0200

    CSV is now imported making use of the new AlignImporter interface

--- src/beast/app/beauti/BeautiAlignmentProvider.java
+++ src/beast/app/beauti/BeautiAlignmentProvider.java
@@ -35,7 +35,6 @@ import beast.evolution.alignment.Alignment;
 import beast.evolution.alignment.FilteredAlignment;
 import beast.evolution.alignment.Sequence;
 import beast.evolution.datatype.DataType;
-import beast.evolution.datatype.FiniteIntegerData;
 import beast.math.distributions.MRCAPrior;
 import beast.util.AddOnManager;
 import beast.util.NexusParser;
@@ -204,104 +203,6 @@ public class BeautiAlignmentProvider extends BEASTObject {
             }
         );
     }
-
-  private Alignment  getCSVData(File file)	{
-    	try {
-    			//Set the number of states to initialize the sequence with.
-    			int min_i = 0;
-    			int max_i = 14;
-    			FiniteIntegerData type = new FiniteIntegerData();
-          type.setInputValue("min_i", min_i);
-          type.setInputValue("max_i", max_i);
-        	type.initAndValidate();
-          
-
-    			// start reading the csv file
-					BufferedReader fin = new BufferedReader(new FileReader(file));
-					
-					// the possible datatypes we can parse from the csv
-					String[] choicesDataType = {"finiteinteger", "nucleotide"};
-					String datatype = (String) JOptionPane.showInputDialog(
-						null,
-						"Choose DataType (default: integer)",
-						"Choose DataType",
-						JOptionPane.PLAIN_MESSAGE,
-						null,
-						choicesDataType,
-						choicesDataType[0]
-						);
-
-					// if the user clicks 'cancel', we go for integer
-					if (datatype == null) {
-						datatype = "finiteinteger";
-						// alert the user of our assumption
-						JOptionPane.showMessageDialog(null, "DataType is assumed integer");
-					}
-
-	        Alignment m_alignment = new Alignment();
-
-	        while (fin.ready()) {
-	        	String line = fin.readLine();
-	        	if ( line.trim().length() == 0 ) {
-   						continue;  // Skip blank lines
-  					}
-	        	String[] row = line.split(",");
-	        	String currentTaxon = row[0];
-
-						if (currentTaxon == null || currentTaxon.trim().length() == 0) {
-							// check if not null and not only white space
-							fin.close();
-							throw new RuntimeException("Expected taxon defined on first line");
-						}
-	        	StringBuilder sb = new StringBuilder();
-	        	int rowLength = row.length;
-	        	
-	        	for (int i = 1; i < row.length; i++) {
-	        		if (datatype == "finiteinteger") {
-	        				int parsedAllel = Integer.parseInt(row[i]);
-	        				if (parsedAllel - min_i < 0) {
-										throw new RuntimeException("Encountered i < min_i");
-									} else if (parsedAllel > max_i) {
-										throw new RuntimeException("Encountered i > max_i");
-									}
-	        				sb.append(parsedAllel);
-	        				sb.append(",");
-	        		} else if (datatype == "nucleotide") {
-	        			sb.append(row[i]);
-	        		}
-	        	}
-	        	String sequenceData = sb.toString();
-	        	Sequence sequence = new Sequence();
-	        	
-	        	if (datatype == "finiteinteger") {
-	        		sequence.init(max_i - min_i + 1, currentTaxon, sequenceData);
-	        	} else if (datatype == "nucleotide") {
-	        		sequence.init(4, currentTaxon, sequenceData);
-	        	}
-	        	sequence.setID("seq_" + currentTaxon);
-	        	m_alignment.sequenceInput.setValue(sequence, m_alignment);
-	        }
-	        fin.close();
-	        
-	        String ID = file.getName();
-	        ID = ID.substring(0, ID.lastIndexOf('.')).replaceAll("\\..*", "");
-	        m_alignment.setID(ID);
-
-	        if (datatype == "finiteinteger") {
-	        	m_alignment.setInputValue("userDataType", type);
-	        } else if (datatype == "nucleotide") {
-	        	m_alignment.dataTypeInput.setValue(datatype, m_alignment);
-	        }
-
-	        m_alignment.initAndValidate();
-	        return m_alignment;
-    		
-    	} catch (Exception e) {
-			e.printStackTrace();
-			JOptionPane.showMessageDialog(null, "Loading of " + file.getName() + " failed: " + e.getMessage());
-    	}
-		return null;
-  }  
 
 	static public BEASTInterface getXMLData(File file) {
 		String xml = "";
